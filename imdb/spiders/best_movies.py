83993c0215445b924d1f40bc27046bf1da5ddbda
yhy940806 <yhy940806@163.com>
2021-07-28 20:53:00 -0400

    update allowed_domains and start_urls

--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -6,16 +6,12 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
-    allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    allowed_domains = ['web.archive.org']
+    start_urls = ['https://web.archive.org/web/20200715000935if_/https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='list-item-header']/a"), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        print(response.url)
